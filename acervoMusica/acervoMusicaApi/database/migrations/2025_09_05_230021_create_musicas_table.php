5777627430235fd78c68122fbf25fae2fcdaf242
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-09-06 21:51:01 -0300

    chore: correcao do tipagem de dados do bancod e dados

--- acervoMusica/acervoMusicaApi/database/migrations/2025_09_05_230021_create_musicas_table.php
+++ acervoMusica/acervoMusicaApi/database/migrations/2025_09_05_230021_create_musicas_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nome');
             $table->string('cantor');
-            $table->string('ano_lancamento');
+            $table->datetime('ano_lancamento');
             $table->string('album');
             $table->timestamps();
         });
